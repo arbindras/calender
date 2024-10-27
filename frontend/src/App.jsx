0752a3c8d2685479fa39d64d2f095162b498d01e
arbindras <157949417+arbindras@users.noreply.github.com>
2024-10-27 22:21:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './Dashboard';
-import CreateEvent from './events/createEvent';
+import CreateEvent from './events/CreateEvent';
 
 function App() {
   return (
