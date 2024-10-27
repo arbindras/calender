5be4e289a51599c3edee0ebf13a927c626f5b28d
arbindras <157949417+arbindras@users.noreply.github.com>
2024-10-27 22:29:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
-
+app.use(bodyParser.json());
 // Connect to DB
 connectDB();
-
+const corsOption = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 // Middleware
-app.use(cors());
+app.use(cors(corsOption));
 app.use(express.json({ extended: false }));
 
 // Routes
