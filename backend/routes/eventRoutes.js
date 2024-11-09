1a8644194d100b213f20624efe1d1380521d21be
arbindras <157949417+arbindras@users.noreply.github.com>
2024-11-09 12:44:09 +0530

    Delete backend directory

--- backend/routes/eventRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createEvent, getEvents } = require("../controllers/eventController");
-const auth = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post("/events", auth, createEvent);
-router.get("/events", auth, getEvents);
-
-module.exports = router;
