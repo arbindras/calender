abbde30a7fd23024ef52efd340831be4ccd63871
arbindras <157949417+arbindras@users.noreply.github.com>
2024-11-09 08:34:12 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createClient } from "@supabase/supabase-js";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
+const supabaseUrl = "https://qmzaklwkqltswmnbpehh.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
   </StrictMode>,
 )
